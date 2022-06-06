b4e618605e902860e88ee711c1148ddfa2b9d8f1
kliantono <kevin.liantono@gmail.com>
2022-06-06 15:35:13 -0700

    add files

--- RecSys-Materials/Supernatural - CollaborativeFiltering/SimpleUserCF.py
+++ RecSys-Materials/Supernatural - CollaborativeFiltering/SimpleUserCF.py
@@ -53,15 +53,39 @@ watched = {}
 for itemID, rating in trainSet.ur[testUserInnerID]:
     watched[itemID] = 1
     
+# # Get top-rated items from similar users:
+# pos = 0
+# for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
+#     if not itemID in watched:
+#         workoutID = trainSet.to_raw_iid(itemID)
+#         print(ml.getWorkoutID(ml.getWorkoutName(int(workoutID))),ml.getWorkoutName(int(workoutID)),ratingSum)
+#         pos += 1
+#         if (pos > 10):
+#             break
+
 # Get top-rated items from similar users:
-pos = 0
+# pos = 0
+rec_list = list()
 for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
     if not itemID in watched:
         workoutID = trainSet.to_raw_iid(itemID)
-        print(ml.getWorkoutID(ml.getWorkoutName(int(workoutID))),ml.getWorkoutName(int(workoutID)),ratingSum)
-        pos += 1
-        if (pos > 10):
-            break
+        rec_list.append((ml.getWorkoutID(ml.getWorkoutName(int(workoutID))),ml.getWorkoutName(int(workoutID)),ratingSum))
+        rec_list = list(set(rec_list))
+        # if len(rec_list) > 100:
+        # # pos += 1
+        # # if (pos > 20):
+        #     break
 
+rec_list.sort(key=lambda x: x[2], reverse=True)
+counter_list = []
 
+rec_num = 0
+for index, rec in enumerate(rec_list):
+    if rec[0:2] not in counter_list:
+        counter_list.append(rec[0:2])
+        rec_num += 1
+        print(f"{rec_num}.", rec[0:2])
+
+    if rec_num == 20:
+        break
 
