1c4e80f24c1a744c7210d565c0b22e85f06e1486
kliantono <kevin.liantono@gmail.com>
2022-06-07 14:47:04 -0700

    add files

---
+++ RecSys-Materials/Supernatural - CollaborativeFiltering/RBMBakeOff.py
@@ -0,0 +1,37 @@
+from WorkoutLens import WorkoutLens
+from RBMAlgorithm import RBMAlgorithm
+from surprise import NormalPredictor
+from Evaluator import Evaluator
+
+import random
+import numpy as np
+
+def LoadWorkoutLensData():
+    ml = WorkoutLens()
+    print("Loading workout ratings...")
+    data = ml.loadWorkoutLensLatestSmall()
+    print("\nComputing workout popularity ranks so we can measure novelty later...")
+    rankings = ml.getPopularityRanks()
+    return (ml, data, rankings)
+
+np.random.seed(0)
+random.seed(0)
+
+# Load up common data set for the recommender algorithms
+(ml, evaluationData, rankings) = LoadWorkoutLensData()
+
+# Construct an Evaluator to, you know, evaluate them
+evaluator = Evaluator(evaluationData, rankings)
+
+#RBM
+RBM = RBMAlgorithm(epochs=20)
+evaluator.AddAlgorithm(RBM, "RBM")
+
+# Just make random recommendations
+Random = NormalPredictor()
+evaluator.AddAlgorithm(Random, "Random")
+
+# Fight!
+evaluator.Evaluate(True)
+
+evaluator.SampleTopNRecs(ml)
